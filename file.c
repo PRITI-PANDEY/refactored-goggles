8cafa6816746cfd609cf79ee00f49aa2d2c2251a
PRITI-PANDEY <58648371+PRITI-PANDEY@users.noreply.github.com>
2019-12-08 11:02:36 +0530

    Create file.c

---
+++ file.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+	struct student 
+	{
+	  int rollno;
+	  char name[20];
+	  double total;
+	};
+	
+	int main() {
+	  struct student s1;
+	  printf("size=%d\n",sizeof(struct student));
+	  
+	  s1.rollno=101;
+	  strcpy(s1.name,"Scott");
+	  s1.total=80;
+	
+	  printf("Rollno=%d, name=%s,marks=%lf\n",
+	       s1.rollno, s1.name, s1.total);
+	  return 0;
+	}
